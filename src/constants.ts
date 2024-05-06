6025d46c04bed827397f36e073e9d93c5547c8db
Nirav Sanghani <nirav.sanghani@gmail.com>
2024-05-06 09:06:11 -0700

    change css method used for grid layout and add prettier plugin for css rule order

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_MAX_X = 16
-export const DEFAULT_MAX_Y = 8
+export const DEFAULT_MAX_X = 20 // should be double the number of hexes due to doubled coordinates
+export const DEFAULT_MAX_Y = 10
